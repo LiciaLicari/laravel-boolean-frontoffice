049896e5428e481be656f44d80ac89665bca250e
“LiciaLicari” <licianbsplicari@gmail.com>
2023-11-24 17:34:43 +0100

    add new repo

---
+++ src/components/Navbar.vue
@@ -0,0 +1,91 @@
+<script>
+
+export default {
+    name: 'Navbar',
+
+
+
+}
+
+</script>
+
+<template>
+    <ul id="navbar" class="nav justify-content-center w-100 shadow">
+
+
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About Us</router-link></li>
+        <li><router-link to="/contacts">Contact Us</router-link></li>
+
+
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+svg {
+    fill: darkorange;
+}
+
+.nav-link {
+    color: darkorange;
+}
+
+.nav {
+    background-color: rgb(58, 48, 46);
+    padding-top: 25px;
+    padding-bottom: 5px;
+}
+
+
+
+ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+ul li {
+    list-style: none;
+    margin: 0 20px;
+    transition: 0.5s;
+}
+
+ul li a {
+    display: block;
+    position: relative;
+    text-decoration: none;
+    padding: 5px;
+    font-size: 18px;
+    font-family: sans-serif;
+    color: white;
+    text-transform: uppercase;
+    transition: 0.5s;
+}
+
+
+
+ul li a:hover {
+    text-decoration: none;
+    color: darkorange;
+}
+
+ul li a:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: white;
+    transition: 0.5s;
+    transform-origin: right;
+    transform: scaleX(0);
+    z-index: -1;
+}
+
+ul li a:hover:before {
+    transition: transform 0.5s;
+    transform-origin: left;
+    transform: scaleX(1);
+}
+</style>
---
+++ src/views/ContactsView.vue
@@ -0,0 +1,90 @@
+<script>
+
+export default {
+    name: 'ContactsView'
+}
+
+</script>
+
+<template>
+    <div  style="background-color: #1e0904;" class="text-light">
+
+        <div class="p-5 mb-4 text-light">
+            <div class="container py-5 text-center">
+                <h1 style="color: darkorange;" class="display-5 fw-bold">Fancy a table?</h1>
+                <p class="fs-4 py-4">Reserve a spot</p>
+                <button class="btn btn_card btn-lg" type="button">
+                    Tell us everything
+                </button>
+            </div>
+        </div>
+    
+        <div class="container text-light">
+            <form action="" class="py-5">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    class="form-control"
+                    placeholder="Your name here"
+                    aria-describedby="nameHelper"
+                    style="background-color: rgb(110, 73, 67); border: 0;"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="phone" class="form-label">Phone</label>
+                    <input
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    class="form-control"
+                    placeholder="Your phone number"
+                    aria-describedby="phoneHelper"
+                    style="background-color: rgb(110, 73, 67); border: 0;"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    class="form-control"
+                    placeholder="Your email here"
+                    aria-describedby="emailHelper"
+                    style="background-color: rgb(110, 73, 67); border: 0;"
+                    />
+                </div>
+    
+                <div class="mb-3">
+                    <label for="message" class="form-label">Message</label>
+                    <textarea
+                    class="form-control"
+                    name="message"
+                    id="message"
+                    rows="3"
+                    placeholder="Your message here..."
+                    style="background-color: rgb(110, 73, 67); border: 0; color: white;"
+                    ></textarea>
+                </div>
+    
+                 <button type="submit" class="btn btn_card">Send</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style>
+input::placeholder {
+  opacity: 0.5;
+  color: #1e0904 !important;
+}
+
+textarea::placeholder {
+  opacity: 0.5;
+  color: #1e0904 !important;
+}
+
+</style>
